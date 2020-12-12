ad8ed59edb1c1087eb7986a5ae5178a7526245e3
Taranjeet Singh <taranjeet.singh.3312@gmail.com>
2020-12-12 23:42:56 +0530

    Add BooleanField, DomainField, UrlField, EmailField fields

--- pycargo/fields.py
+++ pycargo/fields.py
@@ -1,19 +1,35 @@
+import re
 from typing import Optional, Union, List, Callable
 
+import validators
 
-FuncOrFuncList = Optional[Union[List, Callable]]
+# Types
+FuncOrFuncList = Optional[Union[List[Callable], Callable]]
 
-
+# Fields
 class Field:
     """
     Base class for fields. Includes all the required
     attributes and methods for validation
     """
 
+    _creation_index = 0  # For sorting
+
     def __init__(
         self, required: bool = False, validate: FuncOrFuncList = None
     ):
         self.required = required
+        self._register_validators(validate)
+        self._creation_index = Field._creation_index
+        Field._creation_index += 1
+
+    def __str__(self):
+        return f"<Field>"
+
+    def __repr__(self):
+        return f"<Field>"
+
+    def _register_validators(self, validate):
         self.validators = [self.validate_type]
 
         if validate:
@@ -21,12 +37,6 @@ class Field:
                 self.validators.append(validate)
             elif isinstance(validate, list):
                 self.validators + validate
-
-    def __str__(self):
-        return f"<Field>"
-
-    def __repr__(self):
-        return f"<Field>"
 
     def validate_type(self):
         raise NotImplementedError
@@ -77,3 +87,56 @@ class FloatField(Field):
     def validate_type(self, value):
         if not isinstance(value, float) and value is not None:
             return "Value must be float"
+
+
+class BooleanField(Field):
+    def __str__(self):
+        return f"<BooleanField>"
+
+    def __repr__(self):
+        return f"<BooleanField>"
+
+    def validate_type(self, value):
+        if isinstance(value, str):
+            value = value.lower()
+        if value not in ("true", "1", 1, "false", "0", 0):
+            return f"{value} is not a valid boolean value"
+
+
+class DomainField(StringField):
+    def __str__(self):
+        return f"<DomainField>"
+
+    def __repr__(self):
+        return f"<DomainField>"
+
+    def validate_type(self, value):
+        valid = validators.domain(value)
+        if not valid:
+            return "Invalid domain value"
+
+
+class EmailField(StringField):
+    def __str__(self):
+        return f"<EmailField>"
+
+    def __repr__(self):
+        return f"<EmailField>"
+
+    def validate_type(self, value):
+        valid = validators.email(value)
+        if not valid:
+            return "Invalid email"
+
+
+class UrlField(StringField):
+    def __str__(self):
+        return f"<UrlField>"
+
+    def __repr__(self):
+        return f"<UrlField>"
+
+    def validate_type(self, value):
+        valid = validators.url(value)
+        if not valid:
+            return "Invalid url"
