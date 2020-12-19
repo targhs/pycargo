ea24dcb3ddb2349482135a05e39b30ad91078363
Taranjeet Singh <taranjeet.singh.3312@gmail.com>
2020-12-19 23:42:40 +0530

    Add files required for creating pypi package

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pycargo", # Replace with your own username
+    version="0.0.6",
+    author="Taranjeet Singh",
+    author_email="taranjeet.singh.3312@gmail.com",
+    description="Pycargo is a utility to work with loading and exporting data from excel",
+    long_description="Pycargo is a utility to work with loading and exporting data from excel",
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/taranjeet.singh.3312/pycargo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['validators>=0.18.2,<2', 'openpyxl>=3.0.5,<4']
+)
