c0fb52d82c02200618d5687f3095d7a7d9e68ee1
Taranjeet Singh <taranjeet.singh.3312@gmail.com>
2021-01-17 01:01:19 +0530

    Remove leading _ from method names

--- pycargo/spreadsheet.py
+++ pycargo/spreadsheet.py
@@ -49,7 +49,7 @@ class SpreadSheet(metaclass=SpreadSheetMeta):
         classname = self.__class__.__name__
         return f"<{classname} fields({utils.format_dict(self.fields)})>"
 
-    def _get_fields_for_export(
+    def get_fields_for_export(
         self, field_names: IterableStr = None
     ) -> typing.Dict[str, typing.Type[Field]]:
         """Method to return fields for exporting
@@ -65,17 +65,17 @@ class SpreadSheet(metaclass=SpreadSheetMeta):
             field_name: all_fields[field_name] for field_name in field_names
         }
 
-    def _get_header_style(self, field_name: str) -> typing.Type[Style]:
+    def get_header_style(self, field_name: str) -> typing.Type[Style]:
         """Header styles differ depending upon the field is
         required or not. By default, required fields are
         exported with a red background and optional fields
         have a green background.
         """
-        if self._is_field_required(field_name):
+        if self.is_field_required(field_name):
             return required_header_style
         return header_style
 
-    def _write_headers(
+    def write_headers(
         self, sheet: typing.Type[Worksheet], only: IterableStrOrNone = None
     ) -> None:
         """Method to write headers to the excel file.
@@ -83,16 +83,16 @@ class SpreadSheet(metaclass=SpreadSheetMeta):
         This also applies the header styles and add comments
         to the headers if any.
         """
-        fields = self._get_fields_for_export(only)
+        fields = self.get_fields_for_export(only)
         for idx, header in enumerate(fields, start=1):
             value = fields[header].data_key or header
             cell = sheet.cell(column=idx, row=1, value=value)
-            apply_style(cell, self._get_header_style(header))
+            apply_style(cell, self.get_header_style(header))
             comment_text = fields[header].comment
             if comment_text:
                 cell.comment = Comment(comment_text, author="")
 
-    def _is_field_required(self, name: str) -> bool:
+    def is_field_required(self, name: str) -> bool:
         """Checks whether the field is required or not.
         A field is required if it has validate.Required validator.
         """
@@ -102,19 +102,19 @@ class SpreadSheet(metaclass=SpreadSheetMeta):
                 return True
         return False
 
-    def _required_fields(self) -> typing.List[str]:
+    def required_fields(self) -> typing.List[str]:
         """Method to return field names of required fields"""
         return [
             field_name
             for field_name in self.fields
-            if self._is_field_required(field_name)
+            if self.is_field_required(field_name)
         ]
 
-    def _validate_headers(self, headers: IterableStr) -> None:
-        self._check_unexpected_fields(headers)
-        self._check_required_fields(headers)
+    def validate_headers(self, headers: IterableStr) -> None:
+        self.check_unexpected_fields(headers)
+        self.check_required_fields(headers)
 
-    def _check_unexpected_fields(self, headers: IterableStr) -> None:
+    def check_unexpected_fields(self, headers: IterableStr) -> None:
         """Method to check if any unexpected header is given in
         the excel. Raises InvalidHeaderException it finds any.
         """
@@ -124,7 +124,7 @@ class SpreadSheet(metaclass=SpreadSheetMeta):
                     f"Got unexpected field '{header}'"
                 )
 
-    def _check_required_fields(self, headers: IterableStr) -> None:
+    def check_required_fields(self, headers: IterableStr) -> None:
         """Check whether all the required fields are provided
         in the excel sheet or not. If a required field is missing,
         raise InvalidHeaderException.
@@ -132,7 +132,7 @@ class SpreadSheet(metaclass=SpreadSheetMeta):
         data_key_mapping_alt = {
             name: data_key for data_key, name in self.data_key_mapping.items()
         }
-        for field_name in self._required_fields():
+        for field_name in self.required_fields():
             if field_name not in headers:
                 raise exceptions.InvalidHeaderException(
                     f"Required field "
@@ -163,7 +163,7 @@ class SpreadSheet(metaclass=SpreadSheetMeta):
         """
         workbook = Workbook()
         sheet = workbook.active
-        self._write_headers(sheet, only)
+        self.write_headers(sheet, only)
         workbook.save(path)
 
     def load(self, path: str) -> None:
@@ -174,7 +174,7 @@ class SpreadSheet(metaclass=SpreadSheetMeta):
         """
         df = pd.read_excel(path)
         self.df = df.rename(columns=self.data_key_mapping)
-        self._validate_headers(self.df.columns)
+        self.validate_headers(self.df.columns)
 
     def rows(self) -> typing.Type[RowIterator]:
         """This can be used for iterating over the loaded rows.
